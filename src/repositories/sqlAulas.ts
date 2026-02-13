e5b1dc431c4901a1b0d37d57877a535b5b589870
lucasvida <lucasvida@gmail.com>
2026-02-13 16:51:35 -0300

    Ajuste na Query de Aulas

--- src/repositories/sqlAulas.ts
+++ src/repositories/sqlAulas.ts
@@ -3,7 +3,13 @@ import sql from "../config/db.js";
 export const sqlAulas = {
     async getAll() {
         try {
-            const result = await sql `SELECT * FROM aulas`;
+            const query = await sql `SELECT * FROM aulas`;
+            const result = query.map((aula: any) => ({
+                id: aula.id,
+                created_at: aula.created_at,
+                nome_professor: aula.nome_professor,
+                aula: aula.aula,
+            }))
             return result;
         }
         catch (error) {

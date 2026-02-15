79082a3b74121076f7623f4f7914377215907dbb
Lucas Vida <lucasvida@gmail.com>
2026-02-15 17:23:37 -0300

    Ajuste na mensagem de erro 'Metódo não permitido'

--- src/middlewares/handleErros.ts
+++ src/middlewares/handleErros.ts
@@ -3,7 +3,10 @@ import { Request, Response } from "express";
 export const notAllowedMethods = (req: Request, res: Response) => {
     const { method } = req;
     res.status(405).json({
-        message: `Método ${method} não permitido para esta rota`
+        message: `Método ${method} não permitido para esta rota`,
+        status: 405,
+        timestamp: new Date().toISOString(),
+        docs: "https://docs.sabiaedu.ia.br/"
     });
 }
 
